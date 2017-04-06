ad0dbfb786a86a2c03a6744f2ee20a24f9e848e4
Irish-Student <x13114557@student.ncirl.ie>
2017-04-06 23:26:46 +0000

    restoring crash! just need to fix edit page

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,41 +1,59 @@
 class DoctorsController < ApplicationController
-  before_action :logged_in_doctor, only: [:edit, :update]
-  before_action :correct_doctor,   only: [:edit, :update]
+  before_action :logged_in_doctor, only: [:index, :edit, :update,]
+  before_action :correct_doctor,   only: [:edit, :update,]
+  
+
+  def index
+    @doctors = Doctor.all
+  end
+  
   
   #display doctor info
   def show
     @doctor = Doctor.find(params[:id])
   end
+  
+  
   def new
     @doctor = Doctor.new
   end
   
-  #create account or register
-  def create
-    doctor = Doctor.find_by(email: params[:session][:email].downcase)
-    if doctor && doctor.authenticate(params[:session][:password])
-      log_in doctor
-      params[:session][:remember_me] == '1' ? remember(doctor) : forget(doctor)
-      redirect_back_or doctor
-    else
-      flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
-    end      
+  # #create account or register
+  # def create
+  #   doctor = Doctor.find_by(email: params[:session][:email].downcase)
+  #   if doctor && doctor.authenticate(params[:session][:password])
+  #     log_in doctor
+  #     params[:session][:remember_me] == '1' ? remember(doctor) : forget(doctor)
+  #     redirect_back_or doctor
+  #   else
+  #     flash.now[:danger] = 'Invalid email/password combination'
+  #     render 'new'
+  #   end      
     
+  # end
+  
+  def create #take the edit form input variables of the doctor id and check that the parmaters are a match with the databases
+    @doctor = Doctor.new(doctor_params)
+    if @doctor.save
+      flash[:success] = "Profile is updated"
+      redirect_to @doctor
+    else
+      render 'new'
+    end
+  end
+  
+  def edit #change variables of the doctor id
+    @doctor = Doctor.find(params[:id])
   end
   
   def update
     @doctor = Doctor.find(params[:id])
     if @doctor.update_attributes(doctor_params)
-      flash[:success] = "Profile is updated"
+      flash[:success] = "Profile updated"
       redirect_to @doctor
     else
       render 'edit'
     end
-  end
-  
-  def edit #Recall that the id of the doctor is available in the params[:id] variable
-    @doctor = Doctor.find(params[:id])
   end
   
   private
