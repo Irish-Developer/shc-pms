0548c260f5b01e9a206ea96108214dd901d09233
Irish-Student <x13114557@student.ncirl.ie>
2017-04-13 02:11:00 +0000

    starting clinic and appointment models

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,36 @@ Doctor.create!(fname:  "Youcef", lname: "O'Connor",
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   Doctor.create!(fname:  fname, lname: lname,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+              email: email,
+              password:              password,
+              password_confirmation: password)
 end
+
+
+Condition.create!(name:"Dislocated Joint")
+Condition.create!(name:"Brain injury")
+Condition.create!(name:"Cancer-related")
+Condition.create!(name:"Cancer-related Intensive Care")
+Condition.create!(name:"Cold symptoms")
+Condition.create!(name:"Comfortable")
+Condition.create!(name:"Criticle but stable")
+Condition.create!(name:"Deceased")
+Condition.create!(name:"Discharged")
+Condition.create!(name:"Fever")
+Condition.create!(name:"Flu symptoms")
+Condition.create!(name:"Foriegn body")
+Condition.create!(name:"Fracture")
+Condition.create!(name:"Healthy")
+Condition.create!(name:"Heart Failer")
+Condition.create!(name:"Ligiment Damage")
+Condition.create!(name:"Progressing well")
+Condition.create!(name:"Respiratory Failure")
+Condition.create!(name:"Ruptured Brain Anurysm")
+Condition.create!(name:"Satisfactory")
+Condition.create!(name:"Sepsis")
+Condition.create!(name:"Sexual Transmitted Infection")
+Condition.create!(name:"Shock")
+Condition.create!(name:"Stable")
+Condition.create!(name:"Stress")
+Condition.create!(name:"Trauma")
+Condition.create!(name:"Unconsious")
